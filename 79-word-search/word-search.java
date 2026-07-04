96b220df3effa23359750de5c7e6cc68d574b998
M Nithila <vm.nithila@gmail.com>
2026-07-04 15:42:38 +0530

    Time: 144 ms (44.65%) | Memory: 42.8 MB (87.40%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,32 +1,33 @@
 class Solution {
-    int mc, mr;
+    int mr, mc;
     public boolean exist(char[][] board, String word) {
         mr = board.length;
         mc = board[0].length;
         boolean result = false;
         for(int r = 0; r < mr; r++){
             for(int c = 0; c < mc; c++){
-                result = dfs(r,c,board,word, 0);
-                if (result) {
+                result = dfs(r,c,board,word,0);
+                if(result == true){
                     return true;
                 }
             }
         }
-      return false;  
+        return false;
     }
     public boolean dfs(int r, int c, char[][] board, String word, int i){
         if(i == word.length()){
             return true;
         }
-        if(r < 0 || c < 0 || r >= mr || c >= mc || board[r][c] == '#' || board[r][c] != word.charAt(i)){
+        if(r < 0 || c < 0 || r >= mr || c >= mc || 
+                    board[r][c] == '#' || board[r][c] != word.charAt(i)){
             return false;
         }
         board[r][c] = '#';
-        boolean res = dfs(r - 1, c, board, word, i + 1)||
-                      dfs(r, c + 1, board, word, i + 1)||
-                      dfs(r + 1, c, board, word, i + 1)||
-                      dfs(r, c - 1, board, word, i + 1);
+        boolean result = dfs(r - 1, c, board, word, i + 1) ||
+                         dfs(r, c + 1, board, word, i + 1) ||
+                         dfs(r + 1, c, board, word, i + 1) ||
+                         dfs(r, c - 1, board, word, i + 1);
         board[r][c] = word.charAt(i);
-        return res;
+        return result;
     }
 }
