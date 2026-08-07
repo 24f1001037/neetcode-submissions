358d9c0277958d23914f4c48b059afd6ddeb4ee4
M Nithila <vm.nithila@gmail.com>
2026-08-07 16:24:40 +0530

    Time: 13 ms (96.26%) | Memory: 43.4 MB (79.65%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,23 +1,23 @@
 class Solution {
     public String longestPalindrome(String s) {
         int n = s.length();
-        if(n == 1){
-            return s;
-        }
-        boolean[][] dp = new boolean[n][n];
-        int resLength = 0, start = 0;
-        for(int i = n - 1; i >= 0; i--){
-            for(int j = i; j < n; j++){
-            if(s.charAt(i) == s.charAt(j) && (j - i <= 2 || dp[i + 1][j - 1])){
-                dp[i][j] = true;
-                if(j - i + 1 > resLength){
-                    resLength = j - i + 1; 
-                    start = i;
+        int start = 0, maxLen = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= 1; j++) {
+                int low = i;
+                int high = i + j; 
+                while (low >= 0 && high < n && s.charAt(low) == s.charAt(high)) 
+                {
+                    int currLen = high - low + 1;
+                    if (currLen > maxLen) {
+                        start = low;
+                        maxLen = currLen;
+                    }
+                    low--;
+                    high++;
                 }
             }
-            }
         }
-        String result = s.substring(start, start + resLength);
-        return result;
+        return s.substring(start, start + maxLen);
     }
 }
